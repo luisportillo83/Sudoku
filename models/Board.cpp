972ef4736397f5758b477069fa8a72806e65f18d
luis <luis@luis-HP>
2016-12-28 20:58:41 +0100

    Board set value

--- models/Board.cpp
+++ models/Board.cpp
@@ -1,13 +1,24 @@
+#include <cassert>
+#include <math.h>
 #include "Board.h"
 
 namespace models {
 
 const unsigned int Board::NUMBER_OF_REGIONS = 9;
-const unsigned int Board::NUMBER_OF_CELLS = 9 * 9;
+const unsigned int Board::NUMBER_OF_ROWS = 9;
+const unsigned int Board::NUMBER_OF_COLUMNS = 9;
+const unsigned int Board::NUMBER_OF_CELLS = Board::NUMBER_OF_ROWS * Board::NUMBER_OF_COLUMNS;
 
 Board::Board() {
 	//regions = new Region[Board::NUMBER_OF_REGIONS];
 	cells = new Cell[Board::NUMBER_OF_CELLS];
+}
+
+void Board::setValue(unsigned int row, unsigned int column, unsigned int value) {
+	assert(row <= Board::NUMBER_OF_ROWS);
+	assert(column <= Board::NUMBER_OF_COLUMNS);
+
+	cells[(row * Board::NUMBER_OF_COLUMNS) + column].setValue(value);
 }
 
 }
--- models/Board.h
+++ models/Board.h
@@ -9,9 +9,13 @@ namespace models {
 class Board {
 public:
 	Board();
+	void setValue(unsigned int row, unsigned int column, unsigned int value);
 
 	static const unsigned int NUMBER_OF_REGIONS;
+	static const unsigned int NUMBER_OF_ROWS;
+	static const unsigned int NUMBER_OF_COLUMNS;
 	static const unsigned int NUMBER_OF_CELLS;
+
 private:
 	// TODO - Is this needed? - Region * regions;
 	Cell * cells;
