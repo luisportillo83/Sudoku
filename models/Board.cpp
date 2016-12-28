87fe58f6ecbd6f3a06d87cc3252c3bebe98cb87d
luis <luis@luis-HP>
2016-12-28 21:04:59 +0100

    Esqueletos para isCompleted

--- models/Board.cpp
+++ models/Board.cpp
@@ -21,4 +21,27 @@ void Board::setValue(unsigned int row, unsigned int column, unsigned int value) 
 	cells[(row * Board::NUMBER_OF_COLUMNS) + column].setValue(value);
 }
 
+bool Board::isCompleted() {
+	// Check columns
+	// Check rows
+	// Check regions
+	return true;
+}
+
+bool Board::isRowCompleted(unsigned int row) {
+	// TODO
+	return true;
+}
+
+bool Board::isColumnCompleted(unsigned int column) {
+	// TODO
+	return true;
+}
+
+bool Board::isRegionCompleted(unsigned int region) {
+	// TODO
+	// Construir una region y luego llamar a isCompleted() de la region ???
+	return true;
+}
+
 }
--- models/Board.h
+++ models/Board.h
@@ -10,6 +10,7 @@ class Board {
 public:
 	Board();
 	void setValue(unsigned int row, unsigned int column, unsigned int value);
+	bool isCompleted();
 
 	static const unsigned int NUMBER_OF_REGIONS;
 	static const unsigned int NUMBER_OF_ROWS;
@@ -19,6 +20,10 @@ public:
 private:
 	// TODO - Is this needed? - Region * regions;
 	Cell * cells;
+
+	bool isRowCompleted(unsigned int row);
+	bool isColumnCompleted(unsigned int column);
+	bool isRegionCompleted(unsigned int region);
 };
 
 }
