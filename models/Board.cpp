9d00e1f94ec6738a3206bea7fd75a8df28f37f57
luis <luis@luis-HP>
2016-12-29 20:04:52 +0100

    Board::isCompleted()

--- models/Board.cpp
+++ models/Board.cpp
@@ -25,9 +25,24 @@ void Board::setValue(unsigned int row, unsigned int column, unsigned int value) 
 }
 
 bool Board::isCompleted() {
-	// Check columns
-	// Check rows
-	// Check regions
+	for (unsigned int i = 0; i < Board::NUMBER_OF_ROWS; i++) {
+		if (!isRowCompleted(i)) {
+			return false;
+		}
+	}
+
+	for (unsigned int i = 0; i < Board::NUMBER_OF_COLUMNS; i++) {
+		if (!isColumnCompleted(i)) {
+			return false;
+		}
+	}
+
+	for (unsigned int i = 0; i < Board::NUMBER_OF_REGIONS; i++) {
+		if (!isRegionCompleted(i)) {
+			return false;
+		}
+	}
+
 	return true;
 }
 
