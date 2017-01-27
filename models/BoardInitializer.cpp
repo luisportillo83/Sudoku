e00e94843616c489cc64c8c6f6d418e37fe6d12a
l.portillo.hernandez <l.portillo.hernandez@CPX-WGK42I3VO6T.dir.svc.accenture.com>
2017-01-27 17:33:56 +0000

    No console prints of the Board

--- models/BoardInitializer.cpp
+++ models/BoardInitializer.cpp
@@ -20,17 +20,11 @@ void BoardInitializer::initialize(Board * board, DifficultyLevel difficultyLevel
 		}
 	}
 
-	// TODO random doesn't work as it doesn't follow Sudoku rules
-	//std::random_shuffle(table.begin(), table.end());
-
 	for (unsigned int i = 0; i < board->NUMBER_OF_ROWS; i++) {
 		for (unsigned int j = 0; j < board->NUMBER_OF_COLUMNS; j++) {
 			board->setValue(i, j, table.at(i * board->NUMBER_OF_ROWS + j));
-			std::cout << table.at(i * board->NUMBER_OF_ROWS + j) << " ";
 		}
-		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 
 	removeCellsInBoardForLevel(board, difficultyLevel);
 }
